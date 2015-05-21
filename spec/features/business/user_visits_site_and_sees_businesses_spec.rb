2566b05b771b62c53addb757b5ddeff1effb9762
David Daniel <davedan7@gmail.com>
2015-05-20 19:37:43 -0600

    initial user index test working

--- spec/features/business/user_visits_site_and_sees_businesses_spec.rb
+++ spec/features/business/user_visits_site_and_sees_businesses_spec.rb
@@ -12,6 +12,13 @@ RSpec.feature 'user authentication' do
                                    password: 'password')}
 
     it 'shows the businesses' do
+      User.create(name: 'Turing School',
+       username: 'turing',
+       email: 'turing@turing.io',
+       location: 'Denver',
+       role: 1,
+       password: 'password')
+
       # bob = User.create(name: 'Bob', username: 'Bob', email: 'bob@bob.bob', location: 'Denver', role: 0, password: 'password')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(bob)
       visit businesses_path
