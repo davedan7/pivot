8849328df497781569946985ad2715e890913100
Carmer <carmerandrew@gmail.com>
2015-05-29 10:18:42 -0600

    stuff

--- app/controllers/businesses/job_applications_controller.rb
+++ app/controllers/businesses/job_applications_controller.rb
@@ -1,12 +1,12 @@
 class Businesses::JobApplicationsController < ApplicationController
 
   def index
-    @job_applications = JobApplication.by_business.paginate(page: params[:page])
-    # if current_business?
-    #   @job_applications = all_applications.select { |app| app.job.user.id == current_user.id }
-    # elsif current_business_admin?
-    #   @job_applications = all_applications.select { |app| app.job.user.id == current_user.employer.id }
-    # end
+    all_applications = JobApplication.first(200)
+    if current_business?
+      @job_applications = all_applications.select { |app| app.job.user.id == current_user.id }
+    elsif current_business_admin?
+      @job_applications = all_applications.select { |app| app.job.user.id == current_user.employer.id }
+    end
   end
 
 end
