cd0216fed93cf9896341e40bc80f08e7d6204b78
Carmer <carmerandrew@gmail.com>
2015-05-28 22:35:58 -0600

    test coverage

--- spec/features/business/business_admin_can_edit_job_posts_spec.rb
+++ spec/features/business/business_admin_can_edit_job_posts_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "As a business user" do
   it "can edit jobs" do
-    business_admin = create(:business_admin_user)
-    job = create(:job, title: "Engineer", description: "something", user_id: business_admin.id)
     5.times do |x|
+      x = x + 1
       User.create(id: x, name: "business", role: 1, username: "user#{x}", email: "user#{x}@example.com", password: "password", location: "Denver")
     end
+    business_admin = create(:business_admin_user)
+    job = create(:job, title: "Engineer", description: "something", user_id: business_admin.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(business_admin)
 
     visit business_dashboard_path(business: (User.find(business_admin.employer_id)).slug)
+
     click_button "Manage your Job Postings"
     click_link_or_button "Engineer"
     click_link_or_button "Edit"
